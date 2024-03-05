821d8a2a429163235569d59b583d95492bba9b87
jefersonwork <jefersoncosta336@gmail.com>
2024-03-04 21:59:25 -0300

    feat: add routings

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { View } from 'react-native';
+import * as React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
 import History from './src/Pages/History/index'
+import Home from './src/Pages/Home/index'
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View>
-      <History>
-      </History>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Notifications" component={History} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
